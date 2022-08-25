ce498fa8b3bab9955a1e16b8e754c7bb0deb0e4d
John Savage <jsavage1498@gmail.com>
2022-08-24 18:48:08 -0700

    Added master build execution script

--- RMod_Build/RModExecuteBuild.sh
+++ RMod_Build/RModExecuteBuild.sh
@@ -1,4 +1,4 @@
-ARTIFACT_PATH_SOURCE=$(pwd)/RModBuildArtifacts
+ARTIFACT_PATH_SOURCE=$(pwd)/RMod_Build/RModBuildArtifacts
 ARTIFACT_PATH_TARGET=/RuneBase/RModBuildArtifacts
 
 if [ -d $ARTIFACT_PATH_SOURCE ]; then
@@ -7,10 +7,10 @@ fi
 
 mkdir $ARTIFACT_PATH_SOURCE
 
-docker build -t RuneBuilder -f ./RuneBuilder.dockerfile
+docker build -t runebuilder -f ./RMod_Build/RuneBuilder.dockerfile .
 docker run \
 	-e REPOSITORY=https://github.com/dwmx/rmod \
 	-e BUILD_SCRIPT=./RMod_Build/RModContainerBuild.sh \
 	-e ARTIFACT_PATH=$ARTIFACT_PATH_TARGET \
 	--mount type=bind,source=$ARTIFACT_PATH_SOURCE,target=$ARTIFACT_PATH_TARGET \
-	RuneBuilder
+	runebuilder
